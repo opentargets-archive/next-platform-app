b843a5033cd331b1b03cb509ea57fc7093f99df8
Carlos Cruz <me@carcruzcast.com>
2022-02-11 11:07:39 +0000

    initial project structure :rocket:

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(graphql|gql)$/,
+      exclude: /node_modules/,
+      loader: 'graphql-tag/loader',
+    });
+    return config;
+  },
+  webpackDevMiddleware: (config) => {
+    return config;
+  },
+};
