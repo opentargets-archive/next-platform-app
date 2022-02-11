2e648ad6f515d13cc3a3c8fdd7153944fec45ecd
Carlos Cruz <me@carcruzcast.com>
2022-02-11 12:03:26 +0000

    fix: server side render styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,29 @@
+import { useEffect } from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { CacheProvider } from '@emotion/react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
 
-import createEmotionCache from '../src/components/Layout/createEmotionCache';
 import theme from '../src/theme';
 import client from '../src/client';
 import '../styles/globals.css';
 
-const clientSideEmotionCache = createEmotionCache();
-
 function MyApp(props) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, pageProps } = props;
+
+  // Remove the server-side injected CSS.
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <ApolloProvider client={client}>
-      <CacheProvider value={emotionCache}>
-        <MuiThemeProvider theme={createTheme(theme)}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </MuiThemeProvider>
-      </CacheProvider>
+      <MuiThemeProvider theme={createTheme(theme)}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </MuiThemeProvider>
     </ApolloProvider>
   );
 }
